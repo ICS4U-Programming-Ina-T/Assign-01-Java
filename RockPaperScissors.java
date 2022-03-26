1d267e2c8bfdffeee583b6281ef8293bd6dd304c
Ubuntu <ubuntu@ip-172-31-9-112.ec2.internal>
2022-03-26 02:45:19 +0000

    Finished code for the RockPaperScissors.java program.

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -15,7 +15,7 @@ class RockPaperScissors {
     /**
      * Declaring constant for ROCK.
      */
-    private static final int ROCK = 3;
+    private static final int ROCK = 1;
     /**
      * Declaring constant for PAPER.
      */
@@ -23,71 +23,102 @@ class RockPaperScissors {
     /**
      * Declaring constant for SCISSORS.
      */
-    private static final int SCISSORS = 1;
+    private static final int SCISSORS = 3;
+    /**
+     * Declaring constant for lose statement.
+     */
+    private static final String LOSE = "You lose.";
+    /**
+     * Declaring constant for win statement.
+     */
+    private static final String WIN = "You win!";
 
     /**
      * Determines results of game.
-     * 
-     * @param choice is only being accepted
+     *
+     * @param choice is accepted
+     * @param randomNumber is accepted
      * @return results to main function
      */
-    public static String determineResults(int choice, int randomNumber) {
+    public static String gameResults(int choice, int randomNumber) {
         // declaring variables
         String result = "";
 
+        // deterimes tie or winner of game
         if (choice == randomNumber) {
-            result = "Tie Game!";
-            return result;
+            result = "Tie game.";
         } else if (choice == ROCK && randomNumber == PAPER) {
-            result = "You lose.";
-            return result;
+            result = LOSE;
         } else if (choice == PAPER && randomNumber == ROCK) {
-            result = "You win!";
-            return result;
+            result = WIN;
+        } else if (choice == ROCK && randomNumber == SCISSORS) {
+            result = WIN;
+        } else if (choice == SCISSORS && randomNumber == ROCK) {
+            result = LOSE;
         } else if (choice == PAPER && randomNumber == SCISSORS) {
-            result = "You lose.";
-            return result;
+            result = LOSE;
         } else {
-            result = "You win!";
-            return result;
+            result = WIN;
         }
+        return result;
     }
 
     /**
-     * Declaring constant for the random number
+     * Main entry into program.
+     *
+     * @param args nothing passed in
      */
     public static void main(String[] args) {
         // declaring variables
         final Scanner sc = new Scanner(System.in);
         String choiceString = "";
-        int choiceIntUser = 0;
         String resultUser;
-        final int randomNumberComp = (int) (Math.random() * (ROCK - SCISSORS + 1) + SCISSORS);
+        int choiceUserInt;
+        int tries = 0;
+        int randomNumberComp;
+        int gameCounter = 0;
 
-        System.out.println("Today you will be playing a game of rock, paper, scissors!");
-        System.out.println("Your options are 1: rock, 2: paper, or 3: scissors.");
-        System.out.println();
-        
-        while (choiceIntUser != randomNumberComp) {
-            // gets one of the choices from the user
+        System.out.println("Today you will be playing a game of"
+            + "rock, paper, scissors with the computer!");
+        System.out.println("Your options are 1: rock, "
+            + "2: paper, or 3: scissors.\n");
+
+        while (gameCounter != 1) {
+            // gets one of the specified choices from user
             System.out.print("Enter one of the above choices: ");
             choiceString = sc.nextLine();
-    
+
+            // generates random number corresponding to computer choice
+            randomNumberComp =
+                (int) (Math.random() * (SCISSORS - ROCK + 1) + ROCK);
+
             try {
-                choiceIntUser = Integer.parseInt(choiceString);
-                
-                if (choiceIntUser >= SCISSORS && choiceIntUser <= ROCK) {
-                    resultUser = determineResults(choiceIntUser, randomNumberComp);
-                    
-                    System.out.println("The computer chose " + randomNumberComp);
+                choiceUserInt = Integer.parseInt(choiceString);
+
+                if (choiceUserInt >= ROCK && choiceUserInt <= SCISSORS) {
+
+                    // calls function to determine tie game or a winner
+                    resultUser =
+                        gameResults(choiceUserInt, randomNumberComp);
+
+                    System.out.println("The computer chose: "
+                        + randomNumberComp);
                     System.out.println(resultUser + "\n");
+
+                    tries++;
+
+                    // checks if user has won in order to break loop
+                    if (resultUser == WIN) {
+                        System.out.println("It took you "
+                            + tries + " tries to win!");
+                        gameCounter++;
+                    }
                 } else {
-                    System.out.println("This is not a valid choice!");
+                    System.out.println("This is not a valid choice!\n");
                 }
             } catch (IllegalArgumentException exception) {
-                System.out.println("Invalid string option!");
+                System.out.println("This cannot be accepted as a valid number.\n");
             }
         }
-        
     }
 }
